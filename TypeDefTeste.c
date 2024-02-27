522b789e2e8183e5c221e0299c78f4b140846a7a
AntonioBragap <149955562+AntonioBragap@users.noreply.github.com>
2024-02-27 06:59:27 -0400

    Teste - Typedef

---
+++ TypeDefTeste.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main () {
+    typedef float nota;
+ 
+    
+    nota prova1 = 7.0;
+    nota prova2 = 8.5;
+    nota media = (prova1 + prova2)/2;     
+  	
+    
+    printf("A sua media eh: %.2f", media);   
+    
+}
